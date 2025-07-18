c5154704050e1571b48a0fade8b861d0c3257ead
David (Odin) <davidckss@proton.me>
2025-07-18 16:03:52 +0200

    add 'state' member accessibility to use helper

--- src/lib/__state__/controller-use.ts
+++ src/lib/__state__/controller-use.ts
@@ -6,10 +6,33 @@ import { use$ } from "@legendapp/state/react";
 
 export function controllerUse<T extends Record<string, any>>(target: T) {
   return {
-    use<K extends PropertyWhere<T, ObservablePrimitive<any>>>(
+    use<
+      K extends
+        | PropertyWhere<T, ObservablePrimitive<any>>
+        | PropertyWhere<T["state"], ObservablePrimitive<any>>,
+    >(
       key: K,
-    ): UnwrapObservable<T[K]> {
-      return use$(target[key]) as UnwrapObservable<T[K]>;
+    ): K extends PropertyWhere<T, ObservablePrimitive<any>>
+      ? UnwrapObservable<T[K]>
+      : K extends PropertyWhere<T["state"], ObservablePrimitive<any>>
+        ? UnwrapObservable<T["state"][K]>
+        : never {
+      if (key in target && typeof target[key as keyof T] === "object") {
+        return use$(target[key as keyof T]);
+      }
+
+      if (
+        "state" in target &&
+        target.state &&
+        key in target.state &&
+        typeof target.state[key as keyof T["state"]] === "object"
+      ) {
+        return use$(target.state[key as keyof T["state"]]) as any;
+      }
+
+      throw new Error(
+        `Property "${String(key)}" not found in controller or controller.state`,
+      );
     },
   };
 }
